01a1ae5664cf3fc51a8b8f69a42f5dd72635baeb
Ender Ahmet Yurt <enderyurt@gmail.com>
2024-06-02 13:05:25 +0300

    Make enable to add comment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -83,6 +83,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:title, :content)
+      params.require(:post).permit(:title, :content, :published)
     end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,5 @@ class Comment < ApplicationRecord
   validates :body, presence: true
   validates :commenter, presence: true
 
-  belongs_to :article
   belongs_to :post
 end
